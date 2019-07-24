9464ba791223bce5cc287f067e42435cc28dc032
Gian Lucas Cavalcante de Lima <gianlucas@LTSP>
2019-07-24 16:48:17 -0300

    updated

--- dev.sh
+++ dev.sh
@@ -1,5 +1,13 @@
 echo "1:default-one ($SHELL)"; cat /etc/shells | grep -n -v '^#'
-SELECTED=$(read x; sed -n "$x p" /etc/shells)
-echo $SELECTED
+echo -n "Select default shell: "; read choice
+SELECTED=$(sed -n "$choice p" /etc/shells)
+# echo $choice $SELECTED
 
-sed "s/\(DOT_SHELL=\"\)[^\"]*/\1$SELECTED/" tmux.conf | head -n10 > out.dev
+if [ $choice = '1' ]; then
+    sed "s|\(DOT_SHELL=\"\)[^\"]*|\1\$SHELL|" tmux.conf | head -n10
+else
+    sed "s|\(DOT_SHELL=\"\)[^\"]*|\1$SELECTED|" tmux.conf | head -n10
+fi
+
+echo -n "Prefix key binding: "; read choice
+sed "s|\(DOT_TMUX_PREFIX=\"\)[^\"]*|\1$choice|" tmux.conf | head -n10
