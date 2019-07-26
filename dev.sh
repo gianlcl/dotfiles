2f5031e97e7171d526746af3f6edae70265ec157
Gian Lucas Cavalcante de Lima <gianlucas@LTSP>
2019-07-26 15:47:49 -0300

    updated main branch

--- dev.sh
+++
@@ -1,5 +0,0 @@
-echo "1:default-one ($SHELL)"; cat /etc/shells | grep -n -v '^#'
-SELECTED=$(read x; sed -n "$x p" /etc/shells)
-echo $SELECTED
-
-sed "s/\(DOT_SHELL=\"\)[^\"]*/\1$SELECTED/" tmux.conf | head -n10 > out.dev
