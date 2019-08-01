a2c6d3bee34e2a40165f6bf2e24d1cadfff5bd14
Gian Cavalcante <giansama0@gmail.com>
2019-08-01 13:42:36 -0300

    tmux install updated

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ choice () {
     then
         opts[choice]=
     else
-        opts[choice]=+
+        opts[choice]=✓
     fi
 }
 
@@ -53,24 +53,27 @@ do
     then
         case $opt in
             1)
-                echo "Installing Bash configs"
+                echo "[*] Installing Bash configs"
                 if [ -f ~/.bashrc ]
                 then
                     mv ~/.bashrc ~/.bashrc.old
-                    echo "old bashrc backed up with *.old extension"
+                    echo "[*] Old bashrc backed up with *.old extension"
                 fi
                 if [ ! -d ~/.oh-my-bash ]
                 then
                     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
-                    echo "installed oh-my-bash"
+                    echo "[*] Installed oh-my-bash"
                 fi
                 cp ./bash.conf ~/.bashrc
                 . ~/.bashrc
-                echo "bashrc installed"
+                echo "[*] bashrc installed"
                 if [ ! -x "$(command -v fzf)" ]
                 then
-                    echo "Package fzf must be installed"
+                    echo "[*] Installing fzf locally on ~/.fzf"
+                    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+                    ~/.fzf/install
                 fi
+                echo "[✓] Bash configuration done!"
                 ;;
             2)
                 #echo "Installing Vim configs"
@@ -82,40 +85,47 @@ do
                 #cp ./vim.conf ~/.vimrc
                 ;;
             3)
-                echo "Installing Neovim configs"
+                echo "[*] Installing Neovim configs"
                 if [ -f ~/.config/nvim/init.vim ]
                 then
                     mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.old
-                    echo "old Neovim's init.vim backed up to *.old extension"
+                    echo "[*] Old Neovim's init.vim backed up to *.old extension"
                 fi
                 if [ ! -d ~/.config/nvim ]
                 then
                     mkdir ~/.config/nvim
-                    echo "created configurations directory on ~/.config/nvim"
+                    echo "[*] Created configurations directory on ~/.config/nvim"
                 fi
                 cp ./neovim.conf ~/.config/nvim/init.vim
                 if [ ! -f ~/.config/nvim/autoload/plug.vim  ]
                 then
-                    echo "installing vim-plug as Plugin Manager for Neovim"
+                    echo "[*] Installing vim-plug as Plugin Manager for Neovim"
                     curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-                    echo "installed vim-plug"
+                    echo "[!] Installed vim-plug"
                 fi
                 nvim +PlugInstall +qall
-                echo "plugins installed!"
+                echo "[!] Neovim plugins installed"
+                echo "[✓] Neovim configuration done!"
                 ;;
             4)
-                echo "Installing Tmux configs"
+                echo "[*] Installing Tmux configs"
                 if [ -f ~/.tmux.conf ]
                 then
                     mv ~/.tmux.conf ~/.tmux.conf.old
-                    echo "old tmux.conf backed up with *.old extension"
+                    echo "[!] Old tmux.conf backed up with *.old extension"
                 fi
                 cp ./tmux.conf ~/.tmux.conf
                 if [ ! -d ~/.tmux/plugins/tpm ]
                 then
                     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+                    echo "[*] Installed Tmux Plugin Manager (TPM)"
                 fi
-                echo "to install Tmux plugins run Prefix + I (default Prefix is c-x)"
+                tmux source-file -q "$HOME/.tmux.conf"
+                tmux -Ad -s tpm_install
+                tmux run-shell "$HOME/.tmux/plugins/tpm/bindings/install_plugins"
+                tmux kill-session -t tpm_install
+                echo "[!] Tmux plugins installed"
+                echo "[✓] Tmux configuration done!"
                 ;;    
         esac
     fi
